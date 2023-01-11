5370cada5efe3cb8fc9bacf712e8a59b631d0e85
Gideon <gidewadejob@gmail.com>
2023-01-11 20:54:53 +0100

    - css doesnt return 304 - residents changed from 29 to 23 - minor bug fixes on the foundational .html's - begun exception handling on add receipt submission

--- app.py
+++ app.py
@@ -1,18 +1,21 @@
+import sys
+
 from flask import Flask, render_template, url_for, redirect, request
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Kitchen29.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///C:/Users/Gideon/PycharmProjects/Kitchen29/instance/Kitchen29.db'
 # Initialize the database
+
 db = SQLAlchemy(app)
 
 
 # Create db model
 class Kitchen29(db.Model):
     Resident_Number = db.Column(db.Integer, primary_key=True)
-    Resident_Name   = db.Column(db.String(50), nullable=False)
-    Date_created    = db.Column(db.DateTime, default=datetime.utcnow)
+    Resident_Name = db.Column(db.String(50), nullable=False)
+    Date_created = db.Column(db.DateTime, default=datetime.utcnow)
 
     # Create a function to return a string
     def __repr__(self):
@@ -27,10 +30,14 @@ def home():  # put application's code here
 @app.route("/add_receipt", methods=["POST", "GET"])
 def add_receipt():
     if request.method == "POST":
-        amount          = request.form["amount"]
-        resident        = request.form["resident"]
-        receipt_picture = request.form["receipt_picture"]
-        return redirect(url_for("update_receipts", Amount=amount, Resident=resident, Receipt_picture=receipt_picture))
+        try:
+            amount = request.form["amount"]
+            resident = request.form["resident"]
+            receipt_picture = request.form["receipt_picture"]
+            return redirect(url_for("update_receipts", Amount=amount, Resident=resident, Receipt_picture=receipt_picture))
+        except:
+            print("hello")
+
     else:
         return render_template("add_receipt.html")
 
@@ -42,7 +49,33 @@ def half_annually_payment():
 
 @app.route("/receipts")
 def receipts():
-    return render_template("receipts.html")
+    import pymysql
+    import re
+    host = 'localhost'
+    user = 'root'
+    password = ''
+    try:
+        connection = pymysql.connect(host=host, user=user, password=password, db=db, use_unicode=True, charset='utf-8')
+        print('+=========================+')
+        print('|  CONNECTED TO DATABASE  |')
+        print('+=========================+')
+    except Exception as e:
+        sys.exit('error', e)
+
+    cur = connection.curser()
+    cur.execute("SELECT * FROM dataset")
+    data = cur.fetchall()
+    for row in data:
+        id_berita = row[0]
+        judul = row[1]
+        isi = row[2]
+        print('===============================================')
+        print('BERITA KE', id_berita)
+        print('Judul :', judul)
+        print('Isi   :', isi)
+        print('===============================================')
+
+    return render_template("receipts.html", db=db)
 
 
 @app.route("/receipts")
--- static/javascript/index.js
+++ static/javascript/index.js
@@ -1,6 +1,3 @@
 console.log("I print in index.js")
 
 
-let resident = {
-    first
-}
